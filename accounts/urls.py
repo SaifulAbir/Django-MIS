59402a21e599019b3923770d224e124bc634399f
saiful <saiful@ishraak.com>
2019-08-28 18:29:43 +0600

    added all files in git

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from .forms import PrettyAuthenticationForm
+from .views import index
+urlpatterns = [
+    path('dashboard/', index, name = 'dashboard'),
+    path('', auth_views.LoginView.as_view(template_name = 'accounts/login.html', authentication_form = PrettyAuthenticationForm), name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(), name = 'logout')
+]
