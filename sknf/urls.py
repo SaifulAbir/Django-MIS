4ef7b21c9eefe06457a990e5d12079bbb5ae3ebd
saiful <saiful@ishraak.com>
2019-09-02 19:30:45 +0600

    add upazilla model, urls and crud views

--- sknf/urls.py
+++ sknf/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path('district/', include('districts.urls'))
+    path('district/', include('districts.urls')),
+    path('upazilla/', include('upazillas.urls'))
 ]
---
+++ upazillas/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'upazillas'
+
+urlpatterns = [
+    path('add/', views.upazilla_create, name = 'create_upazilla'),
+    path('upazilla_list/', views.UpazillaList.as_view(), name = 'upazilla_list'),
+    path('delete/<int:pk>/', views.upazilla_delete, name = 'delete_upazilla'),
+    path('update/<int:pk>', views.upazilla_update, name = 'update_upazilla')
+
+]
