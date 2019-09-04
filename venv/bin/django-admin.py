adf72c98c16b2eb8c2ae591c15fa777e3363f368
rakib <rakib@ishraak.com>
2019-09-04 12:37:53 +0600

    venv removed from project

--- venv/bin/django-admin.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-#!/home/username/DjangoOfficeProject/Dev/venv/bin/python3
-=======
-#!/home/rakib/Desktop/sknf_project/venv/bin/python3
->>>>>>> feature/03_division
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
