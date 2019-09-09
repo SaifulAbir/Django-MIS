ce2eba5195df6635734c82d50569f61f6a6a4cec
rakib <rakib@ishraak.com>
2019-09-09 11:33:55 +0600

    url added and images loging for event, Sk leader,Headmaster home page.

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,14 +11,14 @@ app_name = 'accounts'
 urlpatterns = [
     path('dashboard/', index, name = 'dashboard'),
     path('', auth_views.LoginView.as_view(template_name = 'accounts/login.html', authentication_form = PrettyAuthenticationForm, redirect_authenticated_user=True), name = 'login'),
-<<<<<<< HEAD
+
     path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
     path('profile/', profile, name='profile'),
     path('events/', events, name='events'),
-    path('headmaster_home/', headmaster_home, name='headmaster_home')
-]
-=======
-    path('logout/', auth_views.LogoutView.as_view(), name = 'logout')
+    path('headmaster_home/', headmaster_home, name='headmaster_home'),
+
+
+    path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
 ]
 
->>>>>>> 5581e81ffe5ed77f7ae3816ec0c343230ad3edce
+
