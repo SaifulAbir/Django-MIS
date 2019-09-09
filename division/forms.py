9d98ea5b0fdda1f1591fa0e1557aa83a6f34d825
Rashed <rashed@ishraak.com>
2019-09-09 11:10:34 +0600

    validation added.

--- division/forms.py
+++ division/forms.py
@@ -8,3 +8,6 @@ class DivisionForm(forms.ModelForm):
     class Meta:
         model = Division
         fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={'pattern': '[a-zA-Z\s]+', 'oninvalid': "setCustomValidity('Please enter on alphabets only. ')", 'style': ''}),
+        }
