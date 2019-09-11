e4dce73f20942dbf7425b71dd3e1983c90e558d9
Rashed <rashed@ishraak.com>
2019-09-11 18:47:07 +0600

    project merged

--- school/forms.py
+++ school/forms.py
@@ -10,16 +10,16 @@ class SchoolForm(forms.ModelForm):
     class Meta:
         model = School
         fields = ['name', 'school_id', 'division', 'district', 'upazilla', 'union', 'address']
-        widgets = {
-            'address': forms.Textarea(attrs={'rows': 4.5, 'cols': 15, 'style': 'height:7.7em;'}),
-            'name': forms.TextInput(
-                attrs={'pattern': '[a-zA-Z\s]+', 'oninvalid': "setCustomValidity('Please enter on alphabets only. ')",
-                       'style': ''}),
-        }
+        # widgets = {
+        #     'address': forms.Textarea(attrs={'rows': 4.5, 'cols': 15, 'style': 'height:7.7em;'}),
+        #     'name': forms.TextInput(
+        #         attrs={'pattern': "[a-zA-Z\s]+", 'oninvalid': "setCustomValidity('Please enter on alphabets only. ')",
+        #                'style': ''}),
+        # }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['district'].queryset = District.objects.none()
-        self.fields['upazilla'].queryset = Upazilla.objects.none()
-        self.fields['union'].queryset = Union.objects.none()
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['district'].queryset = District.objects.none()
+    #     self.fields['upazilla'].queryset = Upazilla.objects.none()
+    #     self.fields['union'].queryset = Union.objects.none()
 
