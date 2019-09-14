c5eb2143b163d7a17fa1cca2fcd2c883cc31aaec
rakib <rakib@ishraak.com>
2019-09-14 10:21:32 +0600

    union forms chagnes

--- unions/forms.py
+++ unions/forms.py
@@ -9,8 +9,3 @@ class UnionForm(forms.ModelForm):
     class Meta:
         model = Union
         fields = ['division','district','upazilla','name']
-        widgets = {
-            'name': forms.TextInput(
-                attrs={'pattern': '[a-zA-Z\s]+', 'oninvalid': "setCustomValidity('Please enter on alphabets only. ')",
-                       'style': ''}),
-        }
