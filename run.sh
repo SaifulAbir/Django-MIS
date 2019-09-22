bd842899bd54343158602294f2331b51dfd03c53
munir <munir@ishraak.com>
2019-09-22 18:34:22 +0600

    Docker installation

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 cd /sknfproject
+export DJANGO_SETTINGS_MODULE="sknf.settings-docker"
 python check_db.py
 python manage.py migrate
 cat <<EOF | python manage.py shell
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(email='admin').exists():
-   user = User.objects.create_superuser('email', 'pass')
+   user = User.objects.create_superuser('admin', '123')
    user.user_type = 1
    user.save()
 EOF
