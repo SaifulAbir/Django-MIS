3e6c9b603b735a4ba3bfda8e755a77f75a8330b1
shoab <shoab@ishraak.com>
2019-09-24 16:08:53 +0600

    drop down bug fix

--- skmembers/models.py
+++ skmembers/models.py
@@ -16,7 +16,7 @@ class_choice=(
 class SkMemberProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='skmember_profile')
     mobile = models.CharField(max_length=11)
-    student_class = models.CharField(max_length=10, choices="class_choice")
+    student_class = models.CharField(max_length=10, choices=class_choice)
     roll = models.CharField(max_length=128)
     school = models.ForeignKey(School, on_delete=models.CASCADE, blank=True, null=True)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
