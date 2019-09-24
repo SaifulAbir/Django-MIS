3cef6c64673e45e3c3ea369a77a1e3b51a9b71f7
shoab <shoab@ishraak.com>
2019-09-24 13:05:10 +0600

    generate pass

--- skleaders/models.py
+++ skleaders/models.py
@@ -6,11 +6,18 @@ from django.utils import timezone
 from accounts.models import User
 from school.models import School
 
+class_choice=(
+    ('six', 'SIX'),
+    ('seven', 'SEVEN'),
+    ('eight', 'EIGHT'),
+    ('nine', 'NINE'),
+    ("ten", 'TEN'),
 
+)
 class SkLeaderProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='skleader_profile')
     mobile = models.CharField(max_length=11)
-    student_class = models.CharField(max_length=128)
+    student_class = models.CharField(max_length=10,choices=class_choice)
     roll = models.CharField(max_length=128)
     school = models.ForeignKey(School, on_delete=models.CASCADE, blank=True, null=True, unique=True)
 
