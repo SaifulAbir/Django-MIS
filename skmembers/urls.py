56f32cc401af29af2044cf2a2702def79adef8bb
Rashed <rashed@ishraak.com>
2019-09-30 17:40:48 +0600

    sk mermbers list data show based on logged in skleader school id

--- skmembers/urls.py
+++ skmembers/urls.py
@@ -7,6 +7,7 @@ app_name = 'skmembers'
 urlpatterns = [
     path('skmember_add/', views.skmember_profile_view, name='skmember_add'),
     path('skmember_list/', views.SkmemberList.as_view(), name = 'skmember_list'),
+    path('skmember_list_for_skleader/', views.SkmemberListforSkLeader.as_view(), name = 'skmember_list_for_sk_leader'),
     path('skmember_detail/<int:pk>/', views.SkMemberDetail.as_view(), name = 'skmember_detail'),
     path('skmember_update/<int:pk>', views.skmember_update, name='skmember_update'),
 ]
