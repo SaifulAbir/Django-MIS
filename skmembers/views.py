0cf4cf9820c72d6c65ae7bb72a6da350099d9bce
Rashed <rashed@ishraak.com>
2019-09-30 18:34:03 +0600

    SK member add update based on school id

--- skmembers/views.py
+++ skmembers/views.py
@@ -8,7 +8,7 @@ from django.views import generic
 from accounts.models import User
 from skleaders.models import SkLeaderProfile
 from skmembers import models
-from skmembers.forms import SkMemberUserForm, SkMemberProfileForm, EditSkMemberUserForm
+from skmembers.forms import SkMemberUserForm, SkMemberProfileForm, EditSkMemberUserForm, SkMemberProfileFormForSkleader
 from skmembers.models import SkMemberProfile
 
 
@@ -35,6 +35,57 @@ def skmember_profile_view(request):
         'profile_form': profile_form,
     })
 
+
+def skmember_profile_view_skleader(request):
+    if request.method == 'POST':
+        user_form = SkMemberUserForm(request.POST, prefix='UF')
+        profile_form = SkMemberProfileFormForSkleader(request.POST, files=request.FILES, prefix='PF')
+
+        if user_form.is_valid() and profile_form.is_valid():
+            id = request.user.id
+            objSkLeader = SkLeaderProfile.objects.get(user_id=id)
+            school_id = objSkLeader.school_id
+
+            user = user_form.save(commit=False)
+            user.user_type = 6
+            user.save()
+            profile = profile_form.save(commit = False)
+            profile.user = user
+            profile.school_id = school_id
+            profile.save()
+            return HttpResponseRedirect("/skmembers/skmember_list_for_skleader/")
+    else:
+        user_form = SkMemberUserForm(prefix='UF')
+        profile_form = SkMemberProfileFormForSkleader(prefix='PF')
+
+    return render(request, 'skmembers/skmember_profile_add_for_skleader.html', {
+        'user_form': user_form,
+        'profile_form': profile_form,
+    })
+
+def skmember_update_for_skleader(request, pk):
+    skmember_profile = get_object_or_404(SkMemberProfile, pk=pk)
+    user_profile = get_object_or_404(User, pk=int(skmember_profile.user.id))
+    if request.method == 'POST':
+        user_form = EditSkMemberUserForm(request.POST, instance=user_profile)
+        profile_form = SkMemberProfileForm(request.POST, request.FILES, instance=skmember_profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save(commit=False)
+            user.save()
+            profile = profile_form.save(commit = False)
+            profile.user = user
+            profile.save()
+            return HttpResponseRedirect("/skmembers/skmember_list_for_skleader/")
+    else:
+        user_form = EditSkMemberUserForm(instance=user_profile)
+        profile_form = SkMemberProfileForm(instance=skmember_profile)
+
+    return render(request, 'skmembers/skmember_profile_add_for_skleader.html', {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'skmember_profile': skmember_profile,
+    })
+
 class SkmemberList(LoginRequiredMixin, generic.ListView):
     login_url = '/'
     model = models.SkMemberProfile
@@ -51,8 +102,6 @@ class SkmemberListforSkLeader(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         loggedinuser = self.request.user.id
         objSkLeader = SkLeaderProfile.objects.get(user_id=loggedinuser)
-        print(objSkLeader.school_id)
-
         queryset = SkMemberProfile.objects.filter(user__user_type__in=[6,],school_id=objSkLeader.school_id)
 
         return queryset
