710754196c6192a7dbb69a3ea4d9b5a3a389888a
munir <munir@ishraak.com>
2019-09-30 12:11:47 +0600

    Admin name added to run.sh file

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ User = get_user_model()
 if not User.objects.filter(email='admin').exists():
    user = User.objects.create_superuser('admin', '123')
    user.user_type = 1
+   user.first_name = 'Admin'
    user.save()
 EOF
 python manage.py runserver 0:8000
