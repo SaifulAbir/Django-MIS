43a79923d575826445b2ed685434f6bd07bc3205
Rashed <rashed@ishraak.com>
2019-10-01 16:03:47 +0600

    aws configuration file added.

--- sknf/settings-aws.py
+++ sknf/settings-aws.py
@@ -3,7 +3,7 @@ from sknf.settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db-sknf',
+        'NAME': 'sknf-db',
         'USER': 'root',
         'PASSWORD': 'Ish1234#',
         'HOST': 'sknf-db.cae55b7vy4vo.us-east-1.rds.amazonaws.com',
