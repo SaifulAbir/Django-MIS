5affd5c460f48505343bff00b7f196fb616fbc4d
shoab <shoab@ishraak.com>
2019-10-19 11:11:25 +0600

    District Upazilla Test Done

--- districts/tests.py
+++ districts/tests.py
@@ -1,51 +1,51 @@
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from django.test import TestCase
-# Create your tests here.
-from django.urls import reverse
 from django.utils import timezone
-from division.models import Division
-from districts.models import District
-from .forms import DistrictForm
-
+from .models import Division,District
+from sknf.validators import check_valid_chars
 
 class DistrictTest(TestCase):
+    def setUp(self):
+        s1 = Division(name='Dhaka')
+        s1.save()
+        self.division = s1
 
-    def create_division(self, name="Dhaka"):
-        return Division.objects.create(name=name, created_date=timezone.now())
+    def test__when_name_is_null__should_raise_error(self):
+        s = District(division=self.division, created_date=timezone.now())
+        with self.assertRaises(ValidationError):
+            s.full_clean()
 
-    def create_district(self, division, name="Lakshmipur"):
-        return District.objects.create(division=division, name=name, created_date=timezone.now())
+    def test__when_Division_is_null__should_raise_error(self):
+        s = District(name='Gazipur', created_date=timezone.now())
+        with self.assertRaises(ValidationError):
+            s.full_clean()
 
-    # models
-    def testDistrict_whenContentIsCorrect_shouldCreateObject(self):
-        division = self.create_division()
-        district = self.create_district(division = division)
-        actual = district.__str__()
-        expected = district.name
-        self.assertTrue(isinstance(district, District))
-        self.assertEqual(actual, expected)
-
-    # views
-    # Valid Data
-    def testDistrict_create_View_whenValidData_shouldResponse200(self):
-        division = self.create_division()
-        response = self.client.post(reverse('districts:create_district'),
-                                    {'division': division, 'name': "Lakshmipur", 'created_date': timezone.now()})
-        actual = response.status_code
-        expected = 200
-        self.assertEqual(actual, expected)
-
-    # Valid Form Data
-    def testDistrictForm_whenValidData_shouldReturnTrue(self):
-        division = self.create_division()
-        form = DistrictForm(data={'division': str(division.id), 'name': "Lakshmipur"})
-        self.assertTrue(form.is_valid())
-
-    # Invalid Form Data
-    def testDistrictForm_whenInValidData_shouldReturnFalse(self):
-        division = self.create_division()
-        form = DistrictForm(data={'division': str(division.id), 'name': ""})
-        self.assertFalse(form.is_valid())
+    def test__when_name_is_empty__should_raise_error(self):
+        s = District(division=self.division, name='', created_date=timezone.now())
+        with self.assertRaises(ValidationError):
+            s.full_clean()
 
 
+    def test__when_name_is_duplicate__should_raise_error(self):
+        s = District(division=self.division, name='Dhaka',created_date=timezone.now())
+        s1 = District(division=self.division, name='Dhaka',created_date=timezone.now())
+        with self.assertRaises(IntegrityError):
+            s.save()
+            s1.save()
 
+    def test__max_length_validation_is__added(self):
+        max_length = District._meta.get_field('name').max_length
+        self.assertEquals(max_length, 128)
 
+    def test__name_is_greater_then_100_character__should_raise_error(self):
+        s = District(
+            division=self.division,
+            name="tesg sdfgsdg sgddft tesg sdfgsdg sgddft tesg sdfgsdg sgddft tesg sdfgsdg sgddft"
+                 " tesg sdfgsdg sgddft tesg sdfgsdg sgddft tesg sdfgsdg sgddft tesg sdfgsdg sgddft tesg sdfgsdg"
+                 " sgddft tesg sdfgsdg sgddft tesg sdfgsdg sgddft tesg sdfgsdg sgddft tesg sdfgsdg sgddft tesg"
+                 " sdfgsdg sgddft sdfgsdg sgddft tesg"
+                 " sdfgsdg sgddft sdfgsdg sgddft tesg"
+                 " sdfgsdg sgddft ", created_date=timezone.now())
+        with self.assertRaises(ValidationError):
+            s.full_clean()
--- school/tests.py
+++ school/tests.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 from django.test import TestCase
 from django.utils import timezone
-from.models import School, Division, District , Upazilla, Union
+from.models import School, Division, District , Upazilla, Union,SchoolPost
 
 class SchoolTest(TestCase):
     def setUp(self):
@@ -48,6 +48,102 @@ class SchoolTest(TestCase):
         with self.assertRaises(IntegrityError):
             sl1.save()
             sl2.save()
+
+    def test__when_school_code_is_duplicate__should_raise_error(self):
+        sl1 = School( name='Mirpur School', school_id = 123)
+        sl2 = School( name='agranee School', school_id = 123)
+        with self.assertRaises(IntegrityError):
+            sl1.save()
+            sl2.save()
+
+    def test__max_length_validation_is__added_for_school_name(self):
+        max_length = School._meta.get_field('name').max_length
+        self.assertEquals(max_length, 264)
+
+    def test__max_length_validation_is__added_for_school_id(self):
+        max_length = School._meta.get_field('school_id').max_length
+        self.assertEquals(max_length, 255)
+
+    def test__max_length_validation_is__added_for_school_address(self):
+        max_length = School._meta.get_field('address').max_length
+        self.assertEquals(max_length, 200)
+
+    def test__when_Division_is_null___should_pass(self):
+        time = timezone.now()
+        s = School( name='Mirpur School',school_id='123', district=self.district,
+                    upazilla=self.upazilla, union = self.union, address='abc', created_date=time,)
+        try:
+            s.full_clean()
+        except:
+            self.fail()
+
+    def test__when_District_is_null___should_pass(self):
+        time = timezone.now()
+        s = School( name='Mirpur School',school_id='123', division=self.division,
+                    upazilla=self.upazilla, union = self.union, address='abc', created_date=time,)
+        try:
+            s.full_clean()
+        except:
+            self.fail()
+
+    def test__when_Upazilla_is_null___should_pass(self):
+        time = timezone.now()
+        s = School( name='Mirpur School',school_id='123', division=self.division,
+                    district=self.district, union = self.union, address='abc', created_date=time,)
+        try:
+            s.full_clean()
+        except:
+            self.fail()
+
+    def test__when_Union_is_null___should_pass(self):
+        time = timezone.now()
+        s = School( name='Mirpur School',school_id='123', division=self.division,
+                    district=self.district, upazilla = self.upazilla, address='abc', created_date=time,)
+        try:
+            s.full_clean()
+        except:
+            self.fail()
+
+    def test__when_address_is_null___should_pass(self):
+        time = timezone.now()
+        s = School( name='Mirpur School',school_id='123', division=self.division,
+                    district=self.district, upazilla = self.upazilla, union=self.union, created_date=time,)
+        try:
+            s.full_clean()
+        except:
+            self.fail()
+
+
+class SchoolPostTest(TestCase):
+
+    def setUp(self):
+        s1 = School(name='Mirpur School', school_id= '123')
+        s1.save()
+        self.school = s1
+
+    def test__when_school_is_null__should_raise_error(self):
+        s = SchoolPost(text = 'Hello', created_date=timezone.now() )
+        with self.assertRaises(ValidationError):
+            s.full_clean()
+
+    def test__when_text_is_null__should_raise_error(self):
+        s = SchoolPost(school=self.school, created_date=timezone.now() )
+        with self.assertRaises(ValidationError):
+            s.full_clean()
+
+    def test__when_text_is_empty__should_raise_error(self):
+        s = SchoolPost(school=self.school, text='', created_date=timezone.now() )
+        with self.assertRaises(ValidationError):
+            s.full_clean()
+
+    def test__max_length_validation_is__added_for_school_name(self):
+        max_length = SchoolPost._meta.get_field('text').max_length
+        self.assertEquals(max_length, 264)
+
+
+
+
+
 
 
 
