53794ceefc711e383821f73d3c62d079e7060b92
saiful <saiful@ishraak.com>
2019-10-19 16:38:21 +0600

    unit test done of events

--- events/models.py
+++ events/models.py
@@ -1,13 +1,20 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
 from django.utils import timezone
 
+from sknf.validators import check_valid_date
+
 
 class Event(models.Model):
     title = models.CharField(max_length=128, null=False)
-    start_date = models.DateTimeField(default=timezone.now)
-    end_date = models.DateTimeField(default=timezone.now)
+    start_date = models.DateTimeField(default=timezone.now, null=False, validators=[check_valid_date])
+    end_date = models.DateTimeField(default=timezone.now, null=False)
+
+    def clean(self):
+        if self.start_date > self.end_date:
+            raise ValidationError("Dates are incorrect")
 
     def __str__(self):
         return self.title
--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,50 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 # Create your tests here.
+from django.utils import timezone
+
+from events.models import Event
+
+
+class EventTests(TestCase):
+
+    ## Event title testing start
+    def test__null_title__should_raise_error(self):
+        event = Event(start_date=timezone.now(), end_date=timezone.now() + timezone.timedelta(days=2))
+        with self.assertRaises(ValidationError):
+            event.full_clean()
+
+    def test__empty_title__should_raise_error(self):
+        event = Event(title='', start_date=timezone.now(), end_date=timezone.now() + timezone.timedelta(days=2))
+        with self.assertRaises(ValidationError):
+            event.full_clean()
+
+    def test__128_plus_char_title__should_raise_error(self):
+        event = Event(
+            title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam nulla porttitor massa id neque aliquam.",
+            start_date=timezone.now(), end_date=timezone.now() + timezone.timedelta(days=2))
+        with self.assertRaises(ValidationError):
+            event.full_clean()
+
+    def test__dash_in_title__should_pass(self):
+        event = Event(title='AA-', start_date=timezone.now(), end_date=timezone.now() + timezone.timedelta(days=2))
+        try:
+            event.full_clean()
+        except:
+            self.fail()
+
+    ## Event date testing start
+    def test__if_end_date_before_start_date__should_raise_error(self):
+        event = Event(
+            title="test", start_date=timezone.now(), end_date=timezone.now() - timezone.timedelta(days=2))
+        with self.assertRaises(ValidationError):
+            event.full_clean()
+
+    def test__start_date_null_validation_added(self):
+        null = Event._meta.get_field('start_date').null
+        self.assertEquals(null, False)
+
+    def test__end_date_null_validation_added(self):
+        null = Event._meta.get_field('end_date').null
+        self.assertEquals(null, False)
