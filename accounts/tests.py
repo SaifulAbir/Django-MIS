d38d8f64ab49ed565a1a2313a3bec1681162419b
shoab <shoab@ishraak.com>
2019-10-20 11:21:52 +0600

    Accounts Test Done

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,46 @@
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from django.test import TestCase
+from django.utils import timezone
+from .models import User
 
-# Create your tests here.
+class UserTest(TestCase):
+    def test__when_Every_field_is_valid___should_pass(self):
+        time = timezone.now()
+        s = User(image='1.jpg', email='A@g.com', password=123)
+        try:
+            s.full_clean()
+        except:
+            self.fail()
+
+    def test__when_Email_field_is_null_will_raise__validation_error(self):
+        s= User(image='1.jpg', password=123)
+
+        with self.assertRaises(ValidationError):
+            s.full_clean()
+
+    def test__when_Email_field_is_empty_will_raise__validation_error(self):
+        s= User(image='1.jpg',email='', password=123)
+
+        with self.assertRaises(ValidationError):
+            s.full_clean()
+
+    def test__when_Password_field_is_null_will_raise__validation_error(self):
+        s= User(image='1.jpg',email='a@g.com')
+
+        with self.assertRaises(ValidationError):
+            s.full_clean()
+
+    def test__when_Password_field_is_empty_will_raise__validation_error(self):
+        s= User(image='1.jpg',email='a@g.com',password='')
+
+        with self.assertRaises(ValidationError):
+            s.full_clean()
+
+    def test__when_email_is_not_unique_should_raise__integrity_error(self):
+        s1=User(image='1.jpg', email='A@g.com', password=123)
+        s2=User(image='1.jpg', email='A@g.com', password=124)
+
+        with self.assertRaises(IntegrityError):
+            s1.save()
+            s2.save()
--- skmembers/tests.py
+++ skmembers/tests.py
@@ -96,10 +96,10 @@ class SkleaderDetailsTest(TestCase):
         s3 = SkMemberProfile(school=self.school,user=self.user,gender='M',student_class='6',
                             roll=10,mobile='018152045',image='a.png',joining_date=timezone.now())
         s3.save()
-        self.skleader = s3
+        self.skmember = s3
 
     def test__when_School_is_null__should_raise_error(self):
-        s = SkmemberDetails(skleader=self.skleader,from_date=timezone.now())
+        s = SkmemberDetails(skmember=self.skmember,from_date=timezone.now())
         with self.assertRaises(ValidationError):
             s.full_clean()
 
@@ -107,23 +107,23 @@ class SkleaderDetailsTest(TestCase):
         blank = SkmemberDetails._meta.get_field('school').blank
         self.assertEquals(blank, False)
 
-    def test__when_Skleader_is_null__should_raise_error(self):
+    def test__when_Skmember_is_null__should_raise_error(self):
         s = SkmemberDetails(school=self.school,from_date=timezone.now())
         with self.assertRaises(ValidationError):
             s.full_clean()
 
     def test__if_blank_is_added_in_headmaster(self):
-        blank = SkmemberDetails._meta.get_field('skleader').blank
+        blank = SkmemberDetails._meta.get_field('skmember').blank
         self.assertEquals(blank, False)
 
     def test__when_From_date_is_null__should_raise_error(self):
-        s = SkmemberDetails(school=self.school,skleader=self.skleader)
+        s = SkmemberDetails(school=self.school,skmember=self.skmember)
         with self.assertRaises(ValidationError):
             s.full_clean()
 
     def test__when_to_date_is_null___should_pass(self):
         time = timezone.now()
-        s = SkmemberDetails( school=self.school, skleader= self.skleader, from_date=time)
+        s = SkmemberDetails( school=self.school, skmember= self.skmember, from_date=time)
         try:
             s.full_clean()
         except:
