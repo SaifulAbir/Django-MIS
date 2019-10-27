5321008cc04700b227989b94d1e3c975da8a082c
shoab <shoab@ishraak.com>
2019-10-27 16:46:14 +0600

    Headmaster, Skmember Export Done

--- skleaders/urls.py
+++ skleaders/urls.py
@@ -7,6 +7,8 @@ app_name = 'skleaders'
 urlpatterns = [
     path('skleader_add/', views.skleader_profile_view, name='skleader_add'),
     path('skleader_list/', views.skleader_list, name = 'skleader_list'),
+    path('skleader_list/<export>/', views.skleader_list, name = 'skleader_list_export'),
+
     path('skleader_detail/<int:pk>/', views.SkleaderDetail.as_view(), name = 'skleader_detail'),
     path('skleader_update/<int:pk>', views.skleader_update, name='skleader_update'),
     path('skleader_school_details_update/', views.skleader_details_update),
