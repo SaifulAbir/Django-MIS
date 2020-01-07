90e31ff44cb1fa84ab395bb7e65658424b714437
saiful <saiful@ishraak.com>
2020-01-07 11:56:47 +0600

    serverside pagination done

--- skmembers/urls.py
+++ skmembers/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path('skmember_update_for_skleader/<int:pk>', views.skmember_update_for_skleader, name='skmember_update_for_skleader'),
     path('skmember_school_details_update/', views.skmember_details_update),
     path('skmember_school_details_update_for_skleader/', views.skmember_details_update_for_skleader),
+    path('pagination/', views.pagination, name = 'pagination'),
 
 ]
