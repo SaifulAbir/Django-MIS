e194b3fedc2cf36ec6f724fcab7d36cc2a3436a4
saiful <saiful@ishraak.com>
2020-01-18 12:54:18 +0600

    unittest and strings are checked for skleader

--- class_orientation/tests.py
+++ class_orientation/tests.py
@@ -68,7 +68,7 @@ class ClassOrientationTest(TestCase):
         self.assertEquals(orientation_update_response['Content-Type'], 'application/json')
 
     def test_when__request_for_peer_education_report_list__should_return_peer_education_list(self):
-        logged_in = self.client.login(username='admin@example.com', password='12345')
+        logged_in = self.client.login(username='01712062768', password='12345')
         self.assertTrue(logged_in)
         peer_education_count = PeerEducation.objects.count()
         topic = Topics.objects.exclude(name='')
@@ -80,13 +80,13 @@ class ClassOrientationTest(TestCase):
                             text='<td>Mirpur School (123)</td>', html=True)
 
     def test_when__search_for_peer_education_report_list__should_return_report_list_page_status_code(self):
-        logged_in = self.client.login(username='admin@example.com', password='12345')
+        logged_in = self.client.login(username='01712062768', password='12345')
         self.assertTrue(logged_in)
         peer_education_response = self.client.get(reverse('peer_education:peer_education_search_list'), follow=True)
         self.assertEquals(peer_education_response.status_code, 200)
 
     def test_when__school_name_is_searched__should_return_respective_peer_education_list(self):
-        logged_in = self.client.login(username='admin@example.com', password='12345')
+        logged_in = self.client.login(username='01712062768', password='12345')
         self.assertTrue(logged_in)
         peer_education_response = self.client.get(reverse('peer_education:peer_education_search_list'),
                                                 data={'name_contains': 'Mirpur School'}, follow=True)
@@ -94,7 +94,7 @@ class ClassOrientationTest(TestCase):
                             text='<td>Mirpur School (123)</td>', html=True)
 
     def test_when__division_name_is_searched__should_return_respective_peer_education_list(self):
-        logged_in = self.client.login(username='admin@example.com', password='12345')
+        logged_in = self.client.login(username='01712062768', password='12345')
         self.assertTrue(logged_in)
         peer_education_response = self.client.get(reverse('peer_education:peer_education_search_list'),
                                                 data={'division_contains': 'Dhaka'}, follow=True)
@@ -102,7 +102,7 @@ class ClassOrientationTest(TestCase):
                             text='<td>Dhaka</td>', html=True)
 
     def test_when__district_name_is_searched__should_return_respective_peer_education_list(self):
-        logged_in = self.client.login(username='admin@example.com', password='12345')
+        logged_in = self.client.login(username='01712062768', password='12345')
         self.assertTrue(logged_in)
         peer_education_response = self.client.get(reverse('peer_education:peer_education_search_list'),
                                                 data={'district_contains': 'Gazipur'}, follow=True)
@@ -110,7 +110,7 @@ class ClassOrientationTest(TestCase):
                             text='<td>Gazipur</td>', html=True)
 
     def test_when__all_are_searched__should_return_respective_peer_education_list(self):
-        logged_in = self.client.login(username='admin@example.com', password='12345')
+        logged_in = self.client.login(username='01712062768', password='12345')
         self.assertTrue(logged_in)
         peer_education_response = self.client.get(reverse('peer_education:peer_education_search_list'),
                                                 data={'name_contains': 'Mirpur School', 'division_contains': 'Dhaka',
